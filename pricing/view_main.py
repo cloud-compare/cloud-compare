d47c5024aac8fe4d1667a15242e668af40489722
John Muth <johnmuth81@gmail.com>
2016-08-17 21:49:06 -0700

    Prototype for new top level page.

--- pricing/view_main.py
+++ pricing/view_main.py
@@ -5,10 +5,42 @@ from django.db.models import Count
 
 from django.db import connection
 
+from pprint import pprint
+
+class VMType():
+    def __init__(self, name, gcp, aws, azure):
+        self.name = name
+        self.aws = aws
+        self.gcp = gcp
+        self.azure = azure
+
+    def __repr__(self):
+        return self.name + " " + str(self.aws) + " " +  str(self.gcp) + " " +  str(self.azure)
+
+    def name(self):
+        return self.name
+
+    def gcp(self):
+        return self.gcp
+
+    def aws(self):
+        return self.aws
+
+    def azure(self):
+        return self.azure
+    
+
 def main(request):
-    items = []
+    vmtypes = []
+    vmtypes.append(VMType('small', True, True, True))
+    vmtypes.append(VMType('standard', True, True, True))
+    vmtypes.append(VMType('high cpu', True, True, True))
+    vmtypes.append(VMType('storage', False, True, False))
+    vmtypes.append(VMType('gpu', False, True, False))
+    pprint(vmtypes)
  
-    args = {'body_title': 'Main', 'items': items}
+    items = {'vmtypes' : vmtypes}
+    args = {'body_title': 'Main', 'vmtypes': vmtypes, 'items': items}
     return render(request, 'bootstrap_main.html', args)
 
 def about(request):
